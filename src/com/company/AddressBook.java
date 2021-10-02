84b7356badeb67f1f40a38d6163871d40a873527
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-02 16:31:30 +0530

    Welcome to Address Book Program

---
+++ src/com/company/AddressBook.java
@@ -0,0 +1,9 @@
+package com.company;
+/*
+Creating Address book
+ */
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
